bae8b71d4761ede6433837d38e0cb324cd333594
Tutku Ayavefe <tutkuayavefe@gmail.com>
2023-05-02 21:17:08 +0200

    window maximize

--- src/test/java/testcases/LaunchBrowser.java
+++ src/test/java/testcases/LaunchBrowser.java
@@ -1,16 +1,22 @@
 package testcases;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+
+import java.awt.*;
 
 public class LaunchBrowser {
 
     public static void main(String[] args) throws InterruptedException {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+
+
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize((int) width, (int) height)); //https://whatismyviewport.com/
+        Page page = browserContext.newPage();
         page.navigate("http://playwright.dev");
         System.out.println(page.title());
         Thread.sleep(2000);
