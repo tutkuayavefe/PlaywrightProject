157c7f9bb957e31f9ade2bef0cdba5b8db7902f2
Tutku Ayavefe <tutkuayavefe@gmail.com>
2023-05-02 21:04:07 +0200

    launch browser case is added with playwright

---
+++ src/test/java/testcases/LaunchBrowser.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://playwright.dev");
+        System.out.println(page.title());
+        Thread.sleep(2000);
+        page.close();
+        playwright.close();
+    }
+}
